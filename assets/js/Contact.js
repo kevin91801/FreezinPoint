7d9d98b5c67575ed763beaa58eb85276bae47a16
Kevin Tran <kevin91801@yahoo.com>
2018-02-14 18:45:21 -0800

    worked on files

---
+++ assets/js/Contact.js
@@ -0,0 +1,14 @@
+// google.maps.event.addDomListener(window, "load", initMap);
+
+// function initMap() {
+//         var uluru = {lat: 34.0629008, lng: -118.13679};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 16,
+//           center: uluru
+//         });
+//         var marker = new google.maps.Marker({
+//           position: uluru,
+//           map: map
+//         });
+// }
+
---
+++ assets/js/Menu.js
@@ -0,0 +1,122 @@
+let modal = document.getElementById("modal");
+
+let modal2 = document.getElementById("modal2");
+
+let modal3 = document.getElementById("modal3");
+
+let modal4 = document.getElementById("modal4");
+
+let modal5 = document.getElementById("modal5");
+
+let modal6 = document.getElementById("modal6");
+
+let modal7 = document.getElementById("modal7");
+
+let modal8 = document.getElementById("modal8");
+
+let modal9 = document.getElementById("modal9");
+
+let modal10 = document.getElementById("modal10");
+
+let modal11 = document.getElementById("modal11");
+
+let modal12 = document.getElementById("modal12");
+
+let modal13 = document.getElementById("modal13");
+
+let btn = document.getElementById("halo");
+
+let btn2 = document.getElementById("strawberry");
+
+let btn3 = document.getElementById("nutella");
+
+let btn4 = document.getElementById("pandan");
+
+let btn5 = document.getElementById("matcha");
+
+let btn6 = document.getElementById("cc");
+
+let btn7 = document.getElementById("chamango");
+
+let btn8 = document.getElementById("smores");
+
+let btn9 = document.getElementById("thai");
+
+let btn10 = document.getElementById("milk");
+
+let btn11 = document.getElementById("caramel");
+
+let btn12 = document.getElementById("fruity");
+
+let btn13 = document.getElementById("mint");
+
+let btn14 = document.getElementById("barnie");
+
+let btn15 = document.getElementById("kookie");
+
+let btn16 = document.getElementById("UP");
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+btn2.onclick = function() {
+    modal2.style.display = "block";
+}
+
+btn3.onclick = function() {
+    modal3.style.display = "block";
+}
+
+btn4.onclick = function() {
+    modal4.style.display = "block";
+}
+
+btn5.onclick = function() {
+    modal5.style.display = "block";
+}
+
+btn6.onclick = function() {
+    modal6.style.display = "block";
+}
+
+btn7.onclick = function() {
+    modal7.style.display = "block";
+}
+
+btn8.onclick = function() {
+    modal8.style.display = "block";
+}
+
+btn9.onclick = function() {
+    modal9.style.display = "block";
+}
+
+btn10.onclick = function() {
+    modal10.style.display = "block";
+}
+
+btn11.onclick = function() {
+    modal11.style.display = "block";
+}
+
+btn12.onclick = function() {
+    modal12.style.display = "block";
+}
+
+btn13.onclick = function() {
+    modal13.style.display = "block";
+}
+
+btn14.onclick = function() {
+    modal14.style.display = "block";
+}
+
+btn15.onclick = function() {
+    modal15.style.display = "block";
+}
+
+btn16.onclick = function() {
+    modal16.style.display = "block";
+}
+
